ec7dc875dcc36dcd3d89b13faa0b6ac603a034a4
hobbyquaker <bastiraff@googlemail.com>
2018-03-27 05:15:54 +0200

    longer inputs

--- benchmark.js
+++ benchmark.js
@@ -1,11 +1,26 @@
 const suite = require('benchmark').Suite();
 
+const base = 'muh';
 const topic = 'test';
 
-suite.add('Plain concatenation', function() {
-    const res = 'test' + '/' + topic;
-}).add('String array join()', function() {
-    const res = ['test', topic].join('/');
+suite.add(' 2 Plain concatenation', function() {
+    const res = base + '/' + topic;
+}).add(' 2 String array join()', function() {
+    const res = [base, topic, base].join('/');
+}).add(' 3 Plain concatenation', function() {
+    const res = base + '/' + topic + '/' + base;
+}).add(' 3 String array join()', function() {
+    const res = [base, topic, base].join('/');
+}).add(' 4 Plain concatenation', function() {
+    const res = base + '/' + topic + '/' + base + '/' + topic;
+}).add(' 4 String array join()', function() {
+    const res = [base, topic, base, topic].join('/');
+}).add('20 Plain concatenation', function() {
+    const res = base + '/' + topic + '/' + base + '/' + topic + '/' + base + '/' + topic + '/' + base + '/' + topic +
+        '/' + base + '/' + topic + '/' + base + '/' + topic + '/' + base + '/' + topic + '/' + base + '/' + topic + '/'
+        + base + '/' + topic + '/' + base + '/' + topic;
+}).add('20 String array join()', function() {
+    const res = [base, topic, base, topic, base, topic, base, topic, base, topic, base, topic, base, topic, base, topic, base, topic, base, topic].join('/');
 }).on('cycle', function(event) {
     console.log(String(event.target));
 }).on('complete', function() {
