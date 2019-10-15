77a4f69ed2ea7b996867689ee5d5c13c879573dc
Simon Christmann <simon@christmann.email>
2019-10-15 15:07:34 +0200

    Add example

---
+++ examples/basic.js
@@ -0,0 +1,22 @@
+// const MqttSmarthome = require('mqtt-smarthome-connect');
+const MqttSmarthome = require('../index.js');
+
+const mqsh = new MqttSmarthome('mqtt://localhost', {
+    will: {topic: 'foo/maintenance/online', payload: 'false', retain: true}
+});
+
+mqsh.on('connect', () => {
+    mqsh.publish('foo/maintenance/online', true, {retain: true});
+});
+mqsh.connect();
+
+mqsh.subscribe('foo/+/bar', (topic, message, wildcardMatch, rawPacket) => {
+    console.log('received', wildcardMatch[0], message)
+});
+
+mqsh.publish('foo/boolean/bar', true);
+mqsh.publish('foo/intNumber/bar', 42);
+mqsh.publish('foo/floatNumber/bar', 3.1415);
+mqsh.publish('foo/string/bar', 'A string.');
+mqsh.publish('foo/array/bar', [1, 2, 3]);
+mqsh.publish('foo/object/bar', {val: 42, ts: Date.now()});
